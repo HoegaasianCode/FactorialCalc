807814a74bbe8703a022d67f83d579903cce251a
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-06-06 04:34:05 +0200

    Initial commit

---
+++ FactorialCalc/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FactorialCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(CalcFactorial(5)); // 120
+        }
+
+        private static int CalcFactorial(int x)
+        {
+            int factorSum = 1;
+            int x1;
+            while (x >= 2)
+            {
+                x1 = x * (x - 1);
+                factorSum *= x1;
+                x -= 2;
+            }
+            return factorSum;
+        }
+    }
+}
